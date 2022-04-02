ae66b6f2a00cb467e419e16190d0c6375a22c72f
Anton Antonov <91306218+MagicFirework@users.noreply.github.com>
2022-04-03 03:25:40 +0600

    added lesson05

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 "use strict";
-
-let title = prompt("Как называется ваш проект?");
-const screens = prompt(
-  "Какие типы экранов нужно разработать?",
-  "Простые, Сложные, Интерактивные"
-);
-const screenPrice = +prompt("Сколько будет стоить данная работа?", "12000");
-const adaptive = confirm("Нужен ли адаптив на сайте?");
-const service1 = prompt("Какой дополнительный тип услуги нужен?");
-const servicePrice1 = +prompt("Сколько это будет стоить?");
-const service2 = prompt("Какой дополнительный тип услуги нужен?");
-const servicePrice2 = +prompt("Сколько это будет стоить?");
+// 1 обьявление переменных
+let title;
+let screens;
+let screenPrice;
+let adaptive;
+let rollback = 10; // процент посреднику
 let allServicePrices;
 let fullPrice;
-let rollback = 10; // процент посреднику
 let servicePercentPrice;
+let service1;
+let service2;
 
+//2 обьявление функций
+const isNumber = function (num) {
+  return !isNaN(parseFloat(num) && isFinite(num));
+};
+
+const asking = function () {
+  title = prompt("Как называется ваш проект?", " кальКУлятоР веРстКи");
+  screens = prompt(
+    "Какие типы экранов нужно разработать?",
+    "Простые, Сложные, Интерактивные"
+  );
+  do {
+    screenPrice = prompt("Сколько будет стоить данная работа?", "");
+  } while (!isNumber(screenPrice));
+
+  adaptive = confirm("Нужен ли адаптив на сайте?");
+};
 const showTypeOf = function (variable) {
   console.log(variable, typeof variable);
 };
@@ -33,32 +45,56 @@ const getRollbackMessage = function (price) {
 };
 
 const getAllServicePrices = function () {
-  return servicePrice1 + servicePrice2;
+  let sum = 0;
+  for (let i = 0; i < 2; i++) {
+    let price = 0;
+
+    if (i === 0) {
+      service1 = prompt("Какой первый тип услуги нужен?", "Первая услуга");
+    } else if (i === 1) {
+      service2 = prompt("Какой второй тип услуги нужен?", "Вторая услуга");
+    }
+
+    do {
+      price = prompt("Сколько это будет стоить?", "");
+    } while (!isNumber(price));
+    sum += +price;
+  }
+  return sum;
 };
-allServicePrices = getAllServicePrices();
 
 function getFullPrice() {
-  return screenPrice + allServicePrices;
+  return +screenPrice + allServicePrices;
 }
-fullPrice = getFullPrice();
 
 const getTitle = function () {
   return (
-    title.replace(" ", "")[0].toUpperCase() +
-    title.replace(" ", "").substring(1).toLowerCase()
+    title.trim()[0].toUpperCase() + title.trim().substring(1).toLowerCase()
   );
 };
-title = getTitle();
 
 const getServicePercentPrices = function () {
   return fullPrice - Math.ceil(fullPrice * (rollback / 100));
 };
+
+//3 Выполняем функции и переопределяем значение переменных
+asking();
+
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
 servicePercentPrice = getServicePercentPrices();
+title = getTitle();
 
 showTypeOf(title);
 showTypeOf(fullPrice);
 showTypeOf(adaptive);
 
+//4 выводим консоль
+
+console.log("Начальная сумма:", +screenPrice);
+console.log("Сумма доп услуг:", allServicePrices);
 console.log(screens.toLowerCase().split(", "));
 console.log(getRollbackMessage(fullPrice));
-console.log(servicePercentPrice + " - сумма с учетом агента");
+console.log("сумма с вычетом агента:", servicePercentPrice);
+
+//6 моя консоль
